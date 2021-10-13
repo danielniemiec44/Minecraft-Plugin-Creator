9300663aff2aa0d992a1f78fa0495ad08363172a
danielniemiec44 <danielniemiec44@wp.pl>
2021-10-13 16:12:30 +0200

    TODO: Fix not opening new stage in jar file

--- Launcher/src/com/mcplugincreator/minecraftplugincreator/JavaApplication.java
+++ Launcher/src/com/mcplugincreator/minecraftplugincreator/JavaApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.image.Image;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import netscape.javascript.JSObject;
 
 import java.net.URL;
@@ -24,20 +25,14 @@ public class JavaApplication {
         System.out.println("test");
     }
 
-    public void showProjects() {
+    public void update() {
         Stage newStage = new Stage();
-        System.out.println("Showing projects...");
-        newStage.setTitle("Open or create new project.");
+        newStage.initStyle(StageStyle.UNDECORATED);
         Main.setIcon(this.getClass(), newStage);
 
-        WebView webView = Main.createWebView("index.html");
+        WebView webView = Main.createWebView("update.html");
 
-        JavaApplication javaApp = new JavaApplication(newStage);
-        WebEngine webEngine = webView.getEngine();
-        JSObject window = (JSObject) webEngine.executeScript("window");
-        window.setMember("app", javaApp);
-
-        Scene scene = new Scene(webView, 800, 600);
+        Scene scene = new Scene(webView, 400, 400);
 
         newStage.setScene(scene);
         newStage.show();
